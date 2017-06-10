d750853bd211e04c9ca951acd6875927a2d41361
otto001 <matteo.ludwig@gmail.com>
2017-06-10 11:18:32 +0200

    WLanTest

---
+++ NodeMCU/WLanTest/WLanTest.ino
@@ -0,0 +1,16 @@
+
+
+
+#define LED     D0        // Led in NodeMCU at pin GPIO16 (D0).
+ 
+//----- Setup function. ------------------------
+void setup() 
+{                
+  pinMode(LED, OUTPUT);   // LED pin as output.    
+}
+
+//----- Loop routine. --------------------------
+void loop() {
+
+
+}
