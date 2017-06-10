df633d98152f0688172705676bf269de477b40d7
Marc Maurer <me@marcmaurer.de>
2017-06-10 13:01:34 +0200

    Added iOS Platform and unfinished settings page with ip address field

---
+++ App/src/pages/settings/settings.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'settings.html'
+})
+export class SettingsPage {
+private ip: string;
+constructor(public navCtrl: NavController, private storage: Storage) {
+this.ip = "";
+
+this.storage.get('ip').then((val) => {
+  console.log(this.ip);
+    this.ip = val;
+  });
+}
+
+saveSettings(){
+  this.storage.set('ip', this.ip);
+  console.log("Saving settings");
+
+
+  this.storage.get('ip').then((val) => {
+  console.log(this.ip);
+    this.ip = val;
+  });
+}
+
+}
